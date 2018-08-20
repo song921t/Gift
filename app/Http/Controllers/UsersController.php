2c6aa6bf85cd8bd24ff3ee63335c8e25803c95aa
vagrant <vagrant@homestead>
2018-08-20 14:04:57 +0000

    Add email configuration

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -75,12 +75,12 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = 'timioxc@yahoo.com';
-        $name = '921t';
+        //$from = 'timioxc@yahoo.com';
+        //$name = '921t';
         $to = $user->email;
         $subject = '感谢注册 Gift 应用！请确认你的邮箱。';
-        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function ($message) use ($to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
 
